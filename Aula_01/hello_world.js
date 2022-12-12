ab7044fe099d5261ee4edf16074ee972007ed168
Bruno Henrique <brunohenrique.travessa@gmail.com>
2022-12-12 09:37:06 -0300

    aula 1 do modulo 3

---
+++ Aula_01/hello_world.js
@@ -0,0 +1,12 @@
+console.log("Olá mundo!")
+
+const semaforo = "amarelo";
+const promessa = new Promise((aceitar, rejeitar) => {
+    if(semaforo == "verde"){
+        aceitar("Siga em frente");
+    } else {
+        rejeitar("Pare");
+    }
+})
+.then((aceitar) => console.log(`${aceitar} pois o sinal está ${semaforo}`))
+.catch((rejeitar) => console.log(`${rejeitar} pois o sinal está ${semaforo}`))
